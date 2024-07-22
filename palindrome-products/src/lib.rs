e4a3aca854a0b69b70c167d63d2cf117ea706f61
Graeme Read <graeme@sigma957.net>
2024-07-22 09:23:49 +0100

    Solution 2 for palindrome-products

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -1,7 +1,3 @@
-#![deny(clippy::all)]
-#![warn(clippy::pedantic)]
-#![warn(clippy::nursery)]
-
 use std::collections::HashSet;
 
 /// `Palindrome` is a newtype which only exists when the contained value is a palindrome number in base ten.
@@ -13,40 +9,34 @@ pub struct Palindrome(u64);
 
 impl Palindrome {
     /// Create a `Palindrome` only if `value` is in fact a palindrome when represented in base ten. Otherwise, `None`.
-    #[must_use]
-    pub const fn new(value: u64) -> Option<Self> {
-        if value <= 9 {
-            return Some(Self(value));
+    pub fn new(value: u64) -> Option<Palindrome> {
+        let mut remaining = value;
+        let mut reversed_value = 0;
+        while remaining > 0 {
+            let first_digit = remaining % 10;
+            remaining /= 10;
+            reversed_value *= 10;
+            reversed_value += first_digit;
         }
 
-        let mut mutable_value = value;
-        let mut new_value = 0;
-        while mutable_value > 0 {
-            let first_digit = mutable_value % 10;
-            mutable_value /= 10;
-            new_value *= 10;
-            new_value += first_digit;
+        if reversed_value == value {
+            Some(Self(value))
+        } else {
+            None
         }
-        if new_value == value {
-            return Some(Self(value));
-        }
-
-        None
     }
 
     /// Get the value of this palindrome.
-    #[must_use]
-    pub const fn into_inner(self) -> u64 {
+    pub fn into_inner(self) -> u64 {
         self.0
     }
 }
 
-#[must_use]
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
     let mut palindromic_numbers = HashSet::new();
 
     for number in min..=max {
-        let products = (number..=max).map(|n| number * n);
+        let products = (number..=max).map(|n| number * n).filter(|n| n % 10 != 0);
         for product in products {
             if let Some(palindrome) = Palindrome::new(product) {
                 palindromic_numbers.insert(palindrome);
