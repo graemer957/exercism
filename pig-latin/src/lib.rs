1f5fae9f838f74fe50aced2d151632bb93c7ba26
Graeme Read <graeme@sigma957.net>
2024-07-27 14:37:44 +0100

    Solution 2 for pig-latin
    Not really a new solution, just added some comments

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -1,3 +1,14 @@
+// This is not really a new iteration, but a note to anyone looking, or my future self
+// that there are two really good, short, solutions:
+// - https://exercism.org/tracks/rust/exercises/pig-latin/solutions/lwshang
+//   Best, I have seen and really easy to understand. Would only make a few small tweaks (that
+//   may only be possible given a newer version of the Rust compiler)
+// - https://exercism.org/tracks/rust/exercises/pig-latin/solutions/potatosalad
+//   Not as easy to follow and not a fan of the `unwrap`, but still less verbose than mine!
+//
+// Reflecting on my solution, having reviewed these, I think it's clear my understanding of the
+// problem isn't what it (should?) could have been. I blame my poor linguistic skills :-)
+
 const VOWEL_SOUNDS: [&str; 7] = ["a", "e", "i", "o", "u", "xr", "yt"];
 const CONSONANT_SOUNDS: [&str; 25] = [
     "thr", "sch", "ch", "qu", "th", "rh", "b", "c", "d", "f", "g", "j", "k", "l", "m", "n", "p",
