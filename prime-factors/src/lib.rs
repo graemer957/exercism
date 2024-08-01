bdca2d0c99b76fe67a4fc90128cd99f334feabe3
Graeme Read <graeme@sigma957.net>
2024-08-01 19:14:34 +0100

    Solution 2 for prime-factors

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,18 +1,16 @@
-use primal::Primes;
-
 pub fn factors(n: u64) -> Vec<u64> {
     let mut prime_factors = vec![];
     let mut remainder = n;
-    let mut all_primes = Primes::all();
+    let mut candidates = 2..;
 
     while remainder > 1 {
-        if let Some(prime) = all_primes.next() {
-            let prime = prime as u64;
+        let Some(candidate) = candidates.next() else {
+            break;
+        };
 
-            while remainder > 1 && remainder % prime == 0 {
-                prime_factors.push(prime);
-                remainder /= prime;
-            }
+        while remainder % candidate == 0 {
+            prime_factors.push(candidate);
+            remainder /= candidate;
         }
     }
 
